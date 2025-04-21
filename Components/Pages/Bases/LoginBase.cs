d40acaf96fc01d347987e23feca009e38aea3287
liuvt <ltvan1108@gmail.com>
2025-04-21 13:26:37 +0700

    add components

---
+++ Components/Pages/Bases/LoginBase.cs
@@ -0,0 +1,99 @@
+﻿namespace ntchecker.Components.Pages.Bases;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using MudBlazor;
+using ntchecker.Data.Entities;
+using ntchecker.Services.Interfaces;
+
+public class LoginBase : ComponentBase
+{
+    [Inject]
+    private IAuthenService authenService { get; set; }
+    [Inject]
+    private NavigationManager nav { get; set; }
+    [Inject]
+    private ISnackbar snackBar { get; set; }
+    protected string token { get; set; }
+    protected string ErrorMessage { get; set; }
+    protected override async Task OnInitializedAsync()
+    {
+        try
+        {
+            // Check authentication state
+            if (await authenService.CheckAuthenState()) nav.NavigateTo("/", true);
+            await base.OnInitializedAsync();
+        }
+        catch (Exception ex)
+        {
+            ErrorMessage = ex.Message;
+        }
+    }
+
+    #region Private to handler with data                             
+    // Login
+    private async Task LoginHandler(LoginUserDto _models)
+    {
+        try
+        {
+            await authenService.Login(_models);
+            snackBar.Add($"Đăng nhập thành công.", Severity.Success);
+
+            // Dừng 3s sau khi chuyển hướng
+            Thread.Sleep(TimeSpan.FromSeconds(3));
+
+            // Chuyển về trang chủ
+            nav.NavigateTo("/", true);
+        }
+        catch (Exception ex)
+        {
+            await ClearEditForm();
+            snackBar.Add(ex.Message, Severity.Error);
+        }
+    }
+    #endregion
+
+    #region EditFrom to login
+    protected LoginUserDto models = new LoginUserDto();
+    protected bool _processing = false;
+    protected string textResult;
+
+    // Submit
+    protected async void OnValidSubmit(EditContext editContext)
+    {
+        _processing = true;
+        // Do something
+        await LoginHandler(models);
+        StateHasChanged();
+    }
+
+    // Clean models
+    protected async Task ClearEditForm()
+    {
+        models = new LoginUserDto();
+        _processing = false;
+        StateHasChanged();
+    }
+    #endregion
+
+    #region MudTextField Password
+    protected bool isShowPassword = false;
+    protected InputType PasswordInput = InputType.Password;
+    protected string PasswordInputIcon = Icons.Material.Filled.VisibilityOff;
+
+    protected async Task ShowPasswordEvent()
+    {
+        if (isShowPassword)
+        {
+            isShowPassword = false;
+            PasswordInputIcon = Icons.Material.Filled.VisibilityOff;
+            PasswordInput = InputType.Password;
+        }
+        else
+        {
+            isShowPassword = true;
+            PasswordInputIcon = Icons.Material.Filled.Visibility;
+            PasswordInput = InputType.Text;
+        }
+    }
+    #endregion
+}
---
+++ Components/Pages/Bases/OrderByHistoryBase.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using ntchecker.Data.Models.GGSheetModels;
+using ntchecker.Extensions;
+using ntchecker.Services.Interfaces;
+
+namespace ntchecker.Components.Pages.Bases;
+public class OrderByHistoryBase : ComponentBase
+{
+    [Parameter]
+    public string userId { get; set; }
+    [Parameter]
+    public string selectdate { get; set; }
+    [Inject]
+    protected IJSRuntime Js { get; set; }
+    [Inject]
+    protected NavigationManager nav { get; set; }
+    [Inject]
+    protected IOrderByHistoryService orderByHistoryService { get; set; }
+
+    protected Revenue revenue { get; set; } = new Revenue();
+    protected Contract _contract { get; set; } = new Contract();
+    protected Timepiece _timepiece { get; set; } = new Timepiece();
+
+    protected string totalAmount { get; set; } = string.Empty; // Tổng hợp đồng và cuốc lẻ
+    protected bool isLoaded = false;
+    protected string dateBill { get; set; } = string.Empty;
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            try
+            {
+                userId = Uri.UnescapeDataString(userId); // Chuyển chuỗi URL sang Tên - Mã Nhân Viên
+
+                // Thông tin Doanh thu và QR chuyển khoản
+                revenue = await GetRevenue(userId, selectdate);
+
+                if (!string.IsNullOrEmpty(revenue.createdAt)) dateBill = "Ngày: " + revenue.createdAt;
+
+                if (revenue == null || revenue.typeCar == "Khoán điện") isLoaded = false; // Không tìm thấy ID hoặc xe khoán điện không xem được phiếu
+                else // Xe điện được xem phiếu
+                {
+                    // Thống tin sheet Hợp đồng
+                    _contract = await GetContract(userId, selectdate);
+                    // Thông tin sheet Đồng hồ
+                    _timepiece = await _GetTimepiece(userId, selectdate);
+
+                    totalAmount = new List<string>
+                    {
+                        _contract.TotalPrice, _timepiece.TotalPrice,
+                    }.ltvSumFieldValues(e => e).ltvVNDCurrency();
+
+                    isLoaded = true; //Xe lên ca
+                }
+                StateHasChanged(); // Cập nhật lại giao diện
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                isLoaded = false;
+            }
+        }
+    }
+
+    private async Task<Revenue> GetRevenue(string _userId, string date)
+    {
+        var rs = await orderByHistoryService.GetRevenue(_userId, date);
+        StateHasChanged();
+        return rs;
+    }
+
+    private async Task<Contract> GetContract(string _userId, string date)
+    {
+        var rs = await orderByHistoryService.GetContract(_userId, date);
+        StateHasChanged();
+        return rs;
+    }
+
+    private async Task<Timepiece> _GetTimepiece(string _userId, string date)
+    {
+        var rs = await orderByHistoryService.GetTimepiece(_userId, date);
+        StateHasChanged();
+        return rs;
+    }
+
+    // Chuyển đổi bảng điều khiển sang chế độ máy tính
+    protected bool isDesktopView { get; set; } = false;
+    protected async Task forceDesktopView()
+    {
+        isDesktopView = !isDesktopView;
+        await Js.InvokeVoidAsync("forceDesktopView");
+        StateHasChanged(); // Cập nhật lại giao diện
+    }
+}
